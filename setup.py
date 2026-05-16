609d455ab5dbf1cf3a7b4996c596ac923fdc8a18
Siddharth Singh <01siddharthsingh01@gmail.com>
2026-05-16 16:24:16 +0530

    reqirements installed

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="siddharth",
+    author_email="01SIDDHARTHSINGH01@GMAIL.COM",
+    packages=find_packages()
+)
